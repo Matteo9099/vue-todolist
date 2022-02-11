4000ee0043db18d9630b94e46563d2c8d1b08779
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-11 15:12:02 +0100

    Aggiunti commenti e funzioni

---
+++ main.js
@@ -0,0 +1,32 @@
+const vue = new Vue (
+    {
+        el: "#app",
+        data: {
+            newtodo: '',
+
+            todos: [
+                { obj: 'Pane', done: true},
+                { obj: 'Latte', done: false},
+                { obj: 'Carne', done: true},
+                { obj: 'Biscotti', done: false},
+            ]
+        },
+        methods: {
+            // creo un metodo che stampi all'interno della lista il valore passato nell'imput
+            addTodo(){
+                if(this.newtodo){
+                    this.todos.push({obj: this.newtodo, done:false});
+                    this.newtodo = "";
+                }
+            },
+            // Funzione che elimina il todo cliccato
+            removeTodo(index){
+                this.todos.splice(index, 1);
+            },
+            // al click dell'item invertire il valore di done
+            toggleElement(index){
+                this.todos[index].done = !this.todos[index].done;
+            }
+        }
+    }
+)
